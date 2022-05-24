32dc8083a6fc60786b3c5186c39be464dfc8d8a9
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2022-05-24 17:47:20 +0200

    Updated the shell script and renamed the test files to align with the changes (NOTE: the corresponding test files now have to be named the same)

--- homeworks/homework_6/tests/execute_tests.sh
+++ homeworks/homework_6/tests/execute_tests.sh
@@ -3,21 +3,32 @@
 # list of files to remove in cleanup step
 remove_file_list=(lex.yy.c y.tab.c y.tab.h out)
 
+
 # only look for yacc files with the prefix "test_"
-for test_name in "test_*.y" 
+for test_name in test_*.y
 do
     # generate test files
     yacc -d $test_name
+
+    # split test_name to retrieve file_name without extention
+
+
+    IFS='.' # SET DELIMITER
+    read -ra ADDR <<< "$test_name"
+    PREFIX=${ADDR[0]}
+    IFS='' # RETURN DELIMITER TO DEFAULT VALUE
+
     lex ast_lexer_1.l y.tab.h
     gcc lex.yy.c y.tab.c -o out
 
-    ./out < test_mulOp_1.txt > test_1_actual.txt
-    if cmp "test_1_actual.txt" "test_1_expected.txt"; then
+    # Provide input and store result in separate file
+    ./out < "$PREFIX.txt" > test_1_actual.txt
+
+    if cmp "$PREFIX"_actual.txt "$PREFIX"_expected.txt; then
         echo "True"
     else
         echo "False"
     fi
-
 
     # cleanup
     for remove_file in ${remove_file_list[@]} 
